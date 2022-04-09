7b6622b9312a251c7d681b1bbbc42eb889c97c94
Hiroki Fujii <hfujii@hisystron.com>
2022-04-09 21:22:18 +0900

    eメールアドレス変更を実装

--- functions/src/authFunctions.ts
+++ functions/src/authFunctions.ts
@@ -42,3 +42,43 @@ runWith({
     throw new functions.https.HttpsError("cancelled", "update password failed");
   }
 });
+
+export const updateEmail = functions.
+runWith({
+  timeoutSeconds: 540,
+  memory: '2GB'
+}).https.onCall(async (data, context) => {
+  const authAdmin = firebaseAuthAdmin.getAuth();
+  const auth = firebaseAuth.getAuth();
+  
+  if (!(data.newEmail)) {
+    throw new functions.https.HttpsError("invalid-argument", "newEmail is not found");
+  }
+  if (!(data.password)) {
+    throw new functions.https.HttpsError("invalid-argument", "password is not found");
+  }
+  
+  // Only allow the password verified.
+  if (context.auth && context.auth.token && context.auth.token.uid) {
+    try {
+      const user = await authAdmin.getUser(context?.auth?.token?.uid);
+      if (user.email) {
+        await firebaseAuth.signInWithEmailAndPassword(auth, user?.email, data?.password);
+      } else {
+        throw new functions.https.HttpsError('permission-denied', "authentication failed");
+      }
+    } catch (error) {
+      throw new functions.https.HttpsError('permission-denied', "authentication failed");
+    }
+  } else {
+    throw new functions.https.HttpsError('permission-denied', "authentication failed");
+  }
+
+
+  //run update email-address. 
+  try {
+    authAdmin.updateUser(context?.auth?.token?.uid, {email: data?.newEmail});
+  } catch (error) {
+    throw new functions.https.HttpsError("cancelled", "update email failed");
+  }
+});
